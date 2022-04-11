2dc4a6dfa466accd0621c0892cdef6d37fb37699
hayashikenzo <hayashikenzo@gmail.com>
2022-04-11 00:19:52 -0300

    html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-	res.send("teste...")
+	res.sendFile('index.html', {root: __dirname });
 });
 
 app.listen(process.env.PORT || 3000, () => {
